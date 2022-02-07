Electrenator/Tabbly

commit 36d8cece3d8381c6998fee8dc5b5ee3ffa13f428

Inproved code styling

src/browser/browser_base.py
@@ -9,7 +9,7 @@ class BrowserBase(ABC):
         this.possible_tab_locations = None
 
     @abstractmethod
-    def isRunning(this) -> bool:
+    def is_running(this) -> bool:
         """
         Checks if this browser is running by searching for the `application_name`
         within the active programs.
@@ -17,7 +17,7 @@ class BrowserBase(ABC):
         Returns:
             true if the process is detected to be active, false if not.
         """
-        if this.application_name == None:
+        if this.application_name is not None:
             raise NotImplementedError()
 
         for process in process_iter():
@@ -29,13 +29,13 @@ class BrowserBase(ABC):
         return False
 
     @abstractmethod
-    def getWindows(this) -> list:
+    def get_windows(this) -> list:
         """
         Returns:
             A list of active browser windows for this browser. A window usually
             has one or more `tabs` within itself.
         """
-        if this.possible_tab_locations == None:
+        if this.possible_tab_locations is not None:
             raise NotImplementedError()
 
         session_files = filesystem.find_files(this.possible_tab_locations[0])
@@ -50,13 +50,13 @@ class BrowserBase(ABC):
         return browser_window_data
 
     @abstractmethod
-    def getTabs(this) -> list:
+    def get_tabs(this) -> list:
         """
         Returns:
             a list of all the active browser tabs within this browser. This is a
             concatenation of all the tabs open within all the windows.
         """
-        browser_window_data = this.getWindows()
+        browser_window_data = this.get_windows()
         browser_tab_data = []
 
         for window in browser_window_data:

src/browser/firefox.py
@@ -1,23 +1,26 @@
-from browser.browserBase import BrowserBase
-import lz4.block
 import json
+import lz4.block
+from browser.browser_base import BrowserBase
+
 
 
 class Firefox(BrowserBase):
     def __init__(this):
+        super().__init__()
         this.application_name = "GeckoMain"
         this.possible_tab_locations = [
-            "~/.mozilla/firefox*/*.default*/sessionstore-backups/recovery.jsonlz4",  # Firefox GNU/Linux (Ubuntu & Fedora tested)
+            # Firefox GNU/Linux (Ubuntu & Fedora tested)
+            "~/.mozilla/firefox*/*.default*/sessionstore-backups/recovery.jsonlz4",
         ]
 
-    def isRunning(this) -> bool:
-        return super().isRunning()
+    def is_running(this) -> bool:
+        return super().is_running()
 
-    def getWindows(this) -> list:
-        return super().getWindows()
+    def get_windows(this) -> list:
+        return super().get_windows()
 
-    def getTabs(this) -> list:
-        return super().getTabs()
+    def get_tabs(this) -> list:
+        return super().get_tabs()
 
     def parse_session_file(this, file_path: str) -> list:
         with open(file_path, "rb") as file:
@@ -32,5 +35,6 @@ class Firefox(BrowserBase):
             browser_data = json.loads(file_data)
             window_data = browser_data.get("windows")
 
-            # browser_data json in at least following format from here -> {"windows": [{"tabs": [...]}]}
-            return window_data if window_data != None else []
+            # browser_data json in at least following format from here
+            # -> {"windows": [{"tabs": [...]}]}
+            return window_data if window_data is not None else []

src/browsers.py
@@ -1,9 +1,10 @@
 from browser.firefox import Firefox
+from browser.chrome import Chrome
 
 
 class Browsers:
-    def countTabs(this) -> int:
-        return len(Firefox().getTabs() if Firefox().isRunning() else [])
+    def count_tabs(this) -> int:
+        return len(Firefox().get_tabs() if Firefox().is_running() else [])
 
-    def countWindows(this) -> int:
-        return len(Firefox().getWindows() if Firefox().isRunning() else [])
+    def count_windows(this) -> int:
+        return len(Firefox().get_windows() if Firefox().is_running() else [])

src/discord_presence.py
@@ -11,7 +11,7 @@ class DiscordPresence:
         this._is_connected = False
         this._update_retries = 0
 
-        this._RPC = Presence(client_id)
+        this._presence_connection = Presence(client_id)
         print("Created Presence instance")
 
         # Try to make a first connection
@@ -30,11 +30,11 @@ class DiscordPresence:
     def resume(this):
         if not this._is_connected:
             try:
-                this._RPC.connect()
+                this._presence_connection.connect()
                 this._is_connected = True
                 print("Connected to Presence")
                 this._update_retries = 0
-            except ConnectionRefusedError or ConnectionResetError:
+            except (ConnectionRefusedError, ConnectionResetError):
                 print(
                     "Unable to connect to Presence (Discord is probably closed)",
                     file=sys.stderr
@@ -51,7 +51,7 @@ class DiscordPresence:
 
         if this._is_connected:
             try:
-                return this._RPC.update(state=state)
+                return this._presence_connection.update(state=state)
             except InvalidID:
                 this._is_connected = False
                 print("Presence suddenly disconnected", file=sys.stderr)
@@ -59,7 +59,7 @@ class DiscordPresence:
                 # Retry adding status
                 return this.update(state)
 
-        if (this._update_retries >= this._max_retries):
+        if this._update_retries >= this._max_retries:
             return None
 
         # Try connecing until this._max_retries
@@ -67,7 +67,8 @@ class DiscordPresence:
         this._update_retries += 1
         print(
             "Trying to reconnect " +
-            f"({this._update_retries} of {this._max_retries} tries)" if this._update_retries != 0 else "",
+            f"({this._update_retries} of {this._max_retries} tries)" if this._update_retries != 0
+            else "",
             file=sys.stderr
         )
         this.resume()
@@ -75,7 +76,7 @@ class DiscordPresence:
 
     def pause(this):
         if this._is_connected:
-            this._RPC.close()
+            this._presence_connection.close()
             this._is_connected = False
             print("Closed connection to Presence")
 

src/main.py
@@ -2,7 +2,7 @@ import time
 import sys
 import os
 from browsers import Browsers
-from discordPresence import DiscordPresence
+from discord_presence import DiscordPresence
 import filesystem
 
 
@@ -17,18 +17,18 @@ class Main:
     def start(this):
         try:
             while True:
-                tab_count = this.browsers.countTabs()
-                this.updateStatus(tab_count)
-                this.logActivity(this.browsers.countWindows(), tab_count)
+                tab_count = this.browsers.count_tabs()
+                this.update_status(tab_count)
+                this.log_activity(this.browsers.count_windows(), tab_count)
                 time.sleep(60)
         except KeyboardInterrupt:
             # Final log update before shutdown
-            this.logActivity(this.browsers.countWindows(),
-                             this.browsers.countTabs())
+            this.log_activity(this.browsers.count_windows(),
+                             this.browsers.count_tabs())
             exit(0)
 
-    def updateStatus(this, tab_count: int):
-        if (tab_count > 0):
+    def update_status(this, tab_count: int):
+        if tab_count > 0:
             this.presence.resume()
             status = (
                 f"Using the power of {tab_count} tab"
@@ -40,12 +40,12 @@ class Main:
             print("No tabs detected")
             this.presence.pause()
 
-    def logActivity(this, window_count: int, tab_count: int):
+    def log_activity(this, window_count: int, tab_count: int):
         seperator = ';'
 
         if os.path.isfile(this.tab_logging):
-            with open(this.tab_logging, "at") as logFile:
-                logFile.write(
+            with open(this.tab_logging, "at", encoding="UTF-8") as log_file:
+                log_file.write(
                     f"{int(time.time())}{seperator}" +
                     f"{window_count}{seperator}" +
                     f"{tab_count}{os.linesep}"
@@ -53,14 +53,14 @@ class Main:
             return
         # File does not exist yet -> make it
         filesystem.assure_location(this.tab_logging)
-        with open(this.tab_logging, "xt") as logFile:
-            logFile.write(
+        with open(this.tab_logging, "xt", encoding="UTF-8") as log_file:
+            log_file.write(
                 f"'UNIX timestamp'{seperator}" +
                 f"'Total window count'{seperator}" +
                 f"'Total tab count'{os.linesep}"
             )
         # File is created -> Now really write log file
-        this.logActivity(window_count, tab_count)
+        this.log_activity(window_count, tab_count)
 
 
 if __name__ == "__main__":

commit 1b197e6bf4d4e9b8845557ba7e7f7579cee788e3

Fixed logic mistake introduced in 36d8cece3d8381c6
Referencing commit 36d8cece3d8381c6998fee8dc5b5ee3ffa13f428

src/browser/browser_base.py
@@ -17,7 +17,7 @@ class BrowserBase(ABC):
         Returns:
             true if the process is detected to be active, false if not.
         """
-        if this.application_name is not None:
+        if this.application_name is None:
             raise NotImplementedError()
 
         for process in process_iter():
@@ -35,7 +35,7 @@ class BrowserBase(ABC):
             A list of active browser windows for this browser. A window usually
             has one or more `tabs` within itself.
         """
-        if this.possible_tab_locations is not None:
+        if this.possible_tab_locations is None:
             raise NotImplementedError()
 
         session_files = filesystem.find_files(this.possible_tab_locations[0])