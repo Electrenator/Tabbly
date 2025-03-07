8529c2d28f9e37353d8ab7c527deb899241221ae
L. de Kimpe <electrenator2000@protonmail.com>
2025-03-07 03:41:38 +0100

    Feat; added update interval arguments

--- src-go/main.go
+++ src-go/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/Electrenator/Tabbly/src-go/browser"
 	"github.com/spf13/pflag"
 )
 
 type Settings struct {
-	verbose bool
-	dryRun  bool
+	verbose        bool
+	dryRun         bool
+	updateInterval uint16
 }
 
 func main() {
@@ -21,17 +23,22 @@ func main() {
 	}
 
 	slog.Info(fmt.Sprintf("Application settings: %+v\n", settings))
-	slog.Info(fmt.Sprintf("Thingy: %+v\n", browser.GetUsageInfo()))
+	for {
+		slog.Info(fmt.Sprintf("Thingy: %+v\n", browser.GetUsageInfo()))
+		time.Sleep(time.Second * time.Duration(settings.updateInterval))
+	}
 }
 
 func initSettings() Settings {
 	verboseFlag := pflag.BoolP("verbose", "v", false, "Verbose logging output")
 	dryRunFlag := pflag.Bool("dryrun", false, "Verbose logging output")
+	intervalFlag := pflag.Uint16("interval", 60, "Time between tab checks in seconds")
 
 	pflag.Parse()
 	settings := Settings{
-		verbose: *verboseFlag,
-		dryRun:  *dryRunFlag,
+		verbose:        *verboseFlag,
+		dryRun:         *dryRunFlag,
+		updateInterval: *intervalFlag,
 	}
 	return settings
 }
